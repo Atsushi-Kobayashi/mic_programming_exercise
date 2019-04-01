73491d69dda6a95a0e03758c4552d1d39fcfe7d2
Atsushi Kobayashi <a.kobayashi.lab@gmail.com>
2019-04-01 19:07:43 +0900

    modify hue invert

--- Opencv_exercise/Opencv_hue_invert/hue_invert.cpp
+++ Opencv_exercise/Opencv_hue_invert/hue_invert.cpp
@@ -19,71 +19,184 @@ cv::Vec3b convertToUchar(std::vector<double> vec) {
 
 
 //memo: pixelごとに関数に入れて処理すべきか，Mat全体を入れて処理させるべきか？
+//std::vector<double> HSVConvertedFromBGR(double B, double G, double R) {
+//	double hue, saturation, value;
+//	double max = 0.0, min = 0.0;
+//
+//	if (B == G && G == R) {
+//		hue = 0;
+//		saturation = 0;
+//		value = B;
+//	}
+//	else if (B >= G && B >= R) {
+//		max = B;
+//		if (G >= R) {
+//			min = R;
+//		}
+//		else {
+//			min = G;
+//		}
+//		hue = 60 * (4 + (G - R) / (max - min));
+//	}
+//	else if (G >= R && G >= B) {
+//		max = G;
+//		if (R >= B) {
+//			min = B;
+//		}
+//		else {
+//			min = R;
+//		}
+//		hue = 60 * (2 + (R - B) / (max - min));
+//	}
+//	else if (R >= B && R >= G) {
+//		max = R;
+//		if (B >= G) {
+//			min = G;
+//		}
+//		else {
+//			min = B;
+//		}
+//		hue = 60 * ((B - G) / (max - min));
+//	}
+//	saturation = 255 * ((max - min) / max);
+//	value = max;
+//
+//	if (hue < 0) {
+//		hue += 360;
+//	}
+//	else if (hue > 359) {
+//		hue -= 360;
+//	}
+//	return std::vector<double> {hue, saturation, value};
+//}
+
+
 std::vector<double> HSVConvertedFromBGR(double B, double G, double R) {
-	double hue, saturation, value;
+	double hue=0.0, saturation=0.0, value=0.0;
 	double max = 0.0, min = 0.0;
 
 	if (B == G && G == R) {
 		hue = 0;
 		saturation = 0;
-		value = B;
+		value = B/255.0;
 	}
-	else if (B >= G && B >= R) {
-		max = B;
+	else if (B <= G && B <= R) {
+		min = B;
 		if (G >= R) {
-			min = R;
+			max = G;
 		}
 		else {
-			min = G;
+			max = R;
 		}
-		hue = 60 * (4 + (G - R) / (max - min));
+		hue = (double)60 * (1 + (G - R) / (max - min));
 	}
-	else if (G >= R && G >= B) {
-		max = G;
+	else if (G <= R && G <= B) {
+		min = G;
 		if (R >= B) {
-			min = B;
+			max = R;
 		}
 		else {
-			min = R;
+			max = B;
 		}
-		hue = 60 * (2 + (R - B) / (max - min));
+		hue = (double)60 * (5 + (R - B) / (max - min));
 	}
-	else if (R >= B && R >= G) {
-		max = R;
+	else if (R <= B && R <= G) {
+		min = R;
 		if (B >= G) {
-			min = G;
+			max = B;
 		}
 		else {
-			min = B;
+			max = G;
 		}
-		hue = 60 * ((B - G) / (max - min));
+		hue = (double)60 * (3+(B - G) / (max - min));
 	}
-	saturation = 255 * ((max - min) / max);
-	value = max;
+	saturation = (max - min) /max;
+	value = max/255;
 
 	if (hue < 0) {
-		hue += 360;
+		hue += 360.0;
 	}
 	else if (hue > 359) {
-		hue -= 360;
+		hue -= 360.0;
 	}
 	return std::vector<double> {hue, saturation, value};
 }
 
+//
+//std::vector<double> BGRConvertedFromHSV(double H, double S, double V) {
+//	//＊0.0で初期化
+//	double B = 0.0, G = 0.0, R = 0.0;
+//	double Hd = 0.0;
+//	double C = 0.0, X = 0.0;
+//	if (S == 0) {
+//		B = V;
+//		G = V;
+//		R = V;
+//	}
+//	else {
+//		Hd = H / 60;
+//		C = S * V;
+//
+//		int f = (int)Hd % 2 - 1;
+//		if (f >= 0) {
+//			X = C * (1 - f);
+//		}
+//		else {
+//			X = C * (1 + f);
+//		}
+//
+//		B = (V - C);
+//		G = (V - C);
+//		R = (V - C);
+//
+//		if (0 <= Hd && Hd < 1) {
+//			B += 0;
+//			G += X;
+//			R += C;
+//		}
+//		else if (1 <= Hd && Hd < 2) {
+//			B += 0;
+//			G += C;
+//			R += X;
+//		}
+//		else if (2 <= Hd && Hd < 3) {
+//			B += X;
+//			G += C;
+//			R += 0;
+//		}
+//		else if (3 <= Hd && Hd < 4) {
+//			B += C;
+//			G += X;
+//			R += 0;
+//		}
+//		else if (4 <= Hd && Hd < 5) {
+//			B += C;
+//			G += 0;
+//			R += X;
+//		}
+//		else if (5 <= Hd && Hd < 6) {
+//			B += X;
+//			G += 0;
+//			R += C;
+//		}
+//	}
+//	std::vector<double> BGR = { B,G,R };
+//	return BGR;
+//}
+
 
 std::vector<double> BGRConvertedFromHSV(double H, double S, double V) {
-	//＊0.0で初期化
 	double B = 0.0, G = 0.0, R = 0.0;
 	double Hd = 0.0;
 	double C = 0.0, X = 0.0;
-	if (S == 0) {
+	if (S == 0.0) {
 		B = V;
 		G = V;
 		R = V;
 	}
 	else {
-		Hd = H / 60;
-		C = S * V;
+		Hd = H / 60.0;
+		C = S*V;
 
 		int f = (int)Hd % 2 - 1;
 		if (f >= 0) {
@@ -128,7 +241,7 @@ std::vector<double> BGRConvertedFromHSV(double H, double S, double V) {
 			R += C;
 		}
 	}
-	std::vector<double> BGR = { B,G,R };
+	std::vector<double> BGR = {255*B,255*G,255*R };
 	return BGR;
 }
 
@@ -142,18 +255,44 @@ void hueInvert(cv::Mat &img) {
 
 		for (int i = 0; i < img_cols; ++i) {
 			std::vector<double> HSV = { 0,0,0 };
-			HSV = HSVConvertedFromBGR(img_row_ptr[i][0], img_row_ptr[i][1], img_row_ptr[i][2]);
+			HSV = HSVConvertedFromBGR((double)img_row_ptr[i][0], (double)img_row_ptr[i][1], (double)img_row_ptr[i][2]);
 
 
 			//hueを反転，またvalueも反転
-			HSV[0] += 180;
+	/*		HSV[0] += 180.0;
 			if (HSV[0] >= 360) {
-				HSV[0] -= 360;
+				HSV[0] -= 360.0;
 			}
+
 			HSV[2] = 255 - HSV[2];
+*/
+
+			/*if (HSV[0] >= 359) {
+				std::cout << "OF @(" << i << ", " << j << ") Hue\n";
+			}
+			if (HSV[1] >= 1) {
+				std::cout << "OF @(" << i << ", " << j << ") Saturation\n";
+			}
+			if (HSV[2] >= 1) {
+				std::cout << "OF @(" << i << ", " << j << ") Value\n";
+			}*/
+
 
 			std::vector<double> BGR = { 0,0,0 };
 			BGR = BGRConvertedFromHSV(HSV[0], HSV[1], HSV[2]);
+
+
+			/*if (BGR[0] >= 255) {
+				std::cout << "OF @(" << i << ", " << j << ") Blue\n";
+			}
+			if (BGR[1] >= 255) {
+				std::cout << "OF @(" << i << ", " << j << ") Green\n";
+			}
+			if (BGR[2] >= 255) {
+				std::cout << "OF @(" << i << ", " << j << ") Red\n";
+			}*/
+
+
 			img_row_ptr[i] = convertToUchar(BGR);
 
 		}
